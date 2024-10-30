cc3a084d673d23cb76ee1d68c3033c6ad824ba6b
Top <tammasorn.tammasorn@mail.kmutt.ac.th>
2024-10-30 17:28:18 +0700

    PATCH

--- server/index.js
+++ server/index.js
@@ -30,11 +30,42 @@ app.post('/user', (req, res) => {
 // path = PUT /user/:id
 app.put('/user/:id', (req, res) => {
     let id = req.params.id
-
+    let updateUser = req.body
     // Find the user with the id
     let selectedIndex = users.findIndex(user => user.id == id)
-    // update user
+    // update user (handle case: null || old value)
+    users[selectedIndex].firstname = updateUser.firstname || users[selectedIndex].firstname
+    users[selectedIndex].lastname = updateUser.lastname || users[selectedIndex].lastname
 
+    res.json({
+        message: 'update ok',
+        user: updateUser,
+        indexUpdate: selectedIndex
+    })
+    // add updated user to users
+
+    res.send(selectedIndex + '')
+});
+
+// path = PATCH /user/:id
+app.patch('/user/:id', (req, res) => {
+    let id = req.params.id
+    let updateUser = req.body
+    // Find the user with the id
+    let selectedIndex = users.findIndex(user => user.id == id)
+    // update user (handle case: null || old value)
+    if (updateUser.firstname) {
+        users[selectedIndex].firstname = updateUser.firstname
+    }
+    if (updateUser.lastname) {
+        users[selectedIndex].lastname = updateUser.lastname
+    }
+
+    res.json({
+        message: 'update ok',
+        user: updateUser,
+        indexUpdate: selectedIndex
+    })
     // add updated user to users
 
     res.send(selectedIndex + '')
