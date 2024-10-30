978e376e4fc8994a5f8f38e73675b64c555e936d
Top <tammasorn.tammasorn@mail.kmutt.ac.th>
2024-10-30 18:15:57 +0700

    Complete: API with Rest API

--- server/index.js
+++ server/index.js
@@ -11,24 +11,40 @@ let counter = 1;
 
 // path = GET /users
 app.get('/users', (req, res) => {
-    res.json(users)
+    const filterUsers = users.map(user => {
+        return {
+            id: user.id,
+            firstname: user.firstname,
+            lastname: user.lastname,
+            fullname: user.firstname + ' ' + user.lastname
+        }
+    })
+    res.json(filterUsers)
 })
 
-// path = POST /user
-app.post('/user', (req, res) => {
+// path = POST /users
+app.post('/users', (req, res) => {
     let user = req.body
     user.id = counter
     counter++
 
     users.push(user)
     res.json({
-        message: 'add ok',
+        message: 'add complete',
         user: user
     })
 });
 
-// path = PUT /user/:id
-app.put('/user/:id', (req, res) => {
+// path = GET /users by id
+app.get('/users/:id', (req, res) => {
+    let id = req.params.id
+    // Find the user with the id
+    let selectedIndex = users.findIndex(user => user.id == id)
+    res.json(users[selectedIndex])
+})
+
+// path = PUT /users/:id
+app.put('/users/:id', (req, res) => {
     let id = req.params.id
     let updateUser = req.body
     // Find the user with the id
@@ -36,9 +52,11 @@ app.put('/user/:id', (req, res) => {
     // update user (handle case: null || old value)
     users[selectedIndex].firstname = updateUser.firstname || users[selectedIndex].firstname
     users[selectedIndex].lastname = updateUser.lastname || users[selectedIndex].lastname
+    users[selectedIndex].age = updateUser.age || users[selectedIndex].lastname
+    users[selectedIndex].gender = updateUser.gender || users[selectedIndex].lastname
 
     res.json({
-        message: 'update ok',
+        message: 'update complete',
         user: updateUser,
         indexUpdate: selectedIndex
     })
@@ -47,30 +65,7 @@ app.put('/user/:id', (req, res) => {
     res.send(selectedIndex + '')
 });
 
-// path = PATCH /user/:id
-app.patch('/user/:id', (req, res) => {
-    let id = req.params.id
-    let updateUser = req.body
-    // Find the user with the id
-    let selectedIndex = users.findIndex(user => user.id == id)
-    // update user (handle case: null || old value)
-    if (updateUser.firstname) {
-        users[selectedIndex].firstname = updateUser.firstname
-    }
-    if (updateUser.lastname) {
-        users[selectedIndex].lastname = updateUser.lastname
-    }
-
-    res.json({
-        message: 'update ok',
-        user: updateUser,
-        indexUpdated: selectedIndex
-    })
-    // add updated user to users
-
-    res.send(selectedIndex + '')
-});
-
+// path = DELETE /users/:id
 app.delete('/users/:id', (req, res) => {
     let id = req.params.id
 
@@ -80,7 +75,7 @@ app.delete('/users/:id', (req, res) => {
     users.splice(selectedIndex, 1)
 
     res.json({
-        message: 'delete ok',
+        message: 'delete complete',
         indexDeleted: selectedIndex
     })
 });
